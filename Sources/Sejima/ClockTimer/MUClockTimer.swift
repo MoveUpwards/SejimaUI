15c25d8f39f3df3853ab75cf6f42dccf12b769f8
Damien <damien@slide-r.com>
2020-09-16 18:02:05 +0200

    Add MUClockTimer

---
+++ Sources/Sejima/ClockTimer/MUClockTimer.swift
@@ -0,0 +1,30 @@
+//
+//  MUClockTimer.swift
+//  
+//
+//  Created by Mac on 16/09/2020.
+//
+
+import SwiftUI
+
+public struct MUClockTimer: View {
+    @ObservedObject private var viewModel: MUClockTimerViewModel
+
+    public init(with viewModel: MUClockTimerViewModel) {
+        self.viewModel = viewModel
+    }
+
+    public var body: some View {
+        Text(viewModel.time)
+    }
+}
+
+struct ClockTimer_Previews: PreviewProvider {
+    static let vm = MUClockTimerViewModel()
+
+    static var previews: some View {
+        MUClockTimer(with: vm).onAppear {
+            vm.start()
+        }
+    }
+}
